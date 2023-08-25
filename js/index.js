5d7278fa3b4e25ba4a5ad8eecf1743237d13fa54
HoneyDon <hj5799032@gmail.com>
2023-08-25 23:21:20 +0500

    Editing some code

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("section");
-  
-    function changeBackgroundColor() {
+  const sections = document.querySelectorAll("section");
+  let isScrolling = false;
+
+  function changeBackgroundColor() {
+    if (!isScrolling) {
+      isScrolling = true;
       sections.forEach((section) => {
         const rect = section.getBoundingClientRect();
         if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
           document.body.style.backgroundColor = section.style.backgroundColor;
         }
       });
+      isScrolling = false;
     }
-  
-    window.addEventListener("scroll", changeBackgroundColor);
-  });
-  
+    requestAnimationFrame(changeBackgroundColor);
+  }
+
+  window.addEventListener("scroll", changeBackgroundColor);
+});
