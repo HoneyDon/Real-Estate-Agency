48e33123e453b065f553e2d0bf2d7b719fab6df1
HoneyDon <hj5799032@gmail.com>
2023-08-31 18:40:36 +0500

    Added Scrolling Transitions:)

---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll(".color-transition");
+  
+    function updateSectionBackground() {
+      sections.forEach(function(section) {
+        const rect = section.getBoundingClientRect();
+        const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+        const scrollPercentage = Math.abs(rect.top / window.innerHeight);
+        const backgroundColor = isVisible ? `rgba(0, 0, 0, ${scrollPercentage})` : "";
+  
+        section.style.backgroundColor = backgroundColor;
+      });
+    }
+  
+    window.addEventListener("scroll", updateSectionBackground);
+  });
+  
