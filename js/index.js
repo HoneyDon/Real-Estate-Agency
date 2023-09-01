ea6bb0c080d16400e4aced728201cd5a2b01b979
HoneyDon <hj5799032@gmail.com>
2023-09-01 14:28:58 +0500

    Formatted my Js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll(".color-transition");
-  
-    function updateSectionBackground() {
-      sections.forEach(function(section) {
-        const rect = section.getBoundingClientRect();
-        const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
-        const scrollPercentage = Math.abs(rect.top / window.innerHeight);
-        const backgroundColor = isVisible ? `rgba(0, 0, 0, ${scrollPercentage})` : "";
-  
-        section.style.backgroundColor = backgroundColor;
-      });
-    }
-  
-    window.addEventListener("scroll", updateSectionBackground);
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".color-transition");
+
+  function updateSectionBackground() {
+    sections.forEach(function (section) {
+      const rect = section.getBoundingClientRect();
+      const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+      const scrollPercentage = Math.abs(rect.top / window.innerHeight);
+      const backgroundColor = isVisible ? `rgba(0, 0, 0, ${scrollPercentage})` : "";
+
+      section.style.backgroundColor = backgroundColor;
+    });
+  }
+
+  window.addEventListener("scroll", updateSectionBackground);
+});
